daf1cf22a33b58bd7b0625c41c5c09a15e15d4fd
Paulo Murakami <ppmura13@gmail.com>
2022-02-03 09:14:04 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import * as database from './database';
+import cors from 'cors';
 
 import { noticiasRoutes } from './routes/noticias.routes';
 
+const allowedOrigins = ['http://localhost:4200'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 database.connect();
 const app = express();
-
+app.use(cors(options));
 app.use(express.json());
-
 app.use(noticiasRoutes);
 
 app.listen('5000', () => console.log("Aplicação Rodando"));
